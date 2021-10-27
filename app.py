c060f4269c4b53da027bf85ca2391d80c88c809b
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2021-10-27 22:27:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import base64, pickle
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -9,9 +10,18 @@ def index():
     return 'Hello, World!'
 
 
+# RCE
 @app.route("/cmd", methods=["GET"])
 def rce():
     if request.args.get('c'):
         return os.popen(request.args.get('c')).read(), 200
 
     return ':P', 400
+
+
+# Unsafe pickle deserialization
+@app.route("/cmd", methods=["POST"])
+def unsafe_pickle():
+    data = base64.urlsafe_b64decode(request.form['cmd'])
+    pickle.loads(data)
+    return '', 204
